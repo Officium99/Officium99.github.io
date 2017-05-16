112a0b5205b5e65dd9047e1bd1385c09ce9d184c
Officium99 <ewarnold331@gmail.com>
2017-05-16 09:23:54 -0600

    Add files via upload

---
+++ Final Project 2/camping_site.js
@@ -0,0 +1,59 @@
+document.getElementById("GSDNP").onclick = function(){
+    document.getElementById("prk_header").innerHTML = "Great Sand Dunes National Park";
+    document.getElementById("prk_info").innerHTML = "Located in Colorado, the Great Sand Dunes National park is a fun experience for all that make the trek out to this park. Visitors have many options, from sledding down smaller dunes to making the climb all the way to the top of Star dune, the highest dune in the park. This climb may be slightly difficult for the inexperienced camper, but for most it should be a relatively easy climb. Overall, we give this park a 4.5/5 stars for the great experience it provides to families and the single camper alike."
+}
+document.getElementById("RMNP").onclick = function(){
+    document.getElementById("prk_header").innerHTML = "Rocky Mountain National Park";
+    document.getElementById("prk_info").innerHTML = "Located in Colorful Colorado, this National Park offers a variety of activites for both the well-experienced solo camper, or the family looking to enjoy a weekend away from the distractions of the world at home. Rocky Mountain National Park’s 415 square miles encompass and protect spectacular mountain environments. Enjoy Trail Ridge Road - which crests at over 12,000 feet including many overlooks to experience the subalpine and alpine worlds - along with over 300 miles of hiking trails, wildflowers, wildlife, starry nights, and fun times. In a world of superlatives, Rocky is on top! Overall, we rate this park 4.5/5 for its prime location and endless list of things to do and see."
+}
+document.getElementById("GGCSP").onclick = function(){
+    document.getElementById("prk_header").innerHTML = "Golden Gate Canyon State Park";
+    document.getElementById("prk_info").innerHTML = "More than 12,000 acres of dense forest, rocky peaks, and aspen-rimmed meadows laced with miles of trails awaiting the hiker, horseback rider, mountain biker and winter sports enthusiast at Golden Gate Canyon State Park. Only 30 miles from Denver, Golden Gate Canyon offers such amenities as a electrical hook-ups and tent-sites in two different campgrounds, stocked fishing ponds, picnic sites and the Panorama Point Scenic Overlook, where visitors can see 100 miles of the Continental Divide. The park's numerous group facilities can host several types of events, from weddings to family reunions and company picnics. Besides camping, overnight guests can also stay at one of the five cabins and two yurts at Golden Gate. Overall, we give this park a 4/5 for the great environment ir provides to its visitors."
+}
+document.getElementById("YSNP").onclick = function(){
+    document.getElementById("prk_header").innerHTML = "Yellowstone National Park";
+    document.getElementById("prk_info").innerHTML = "Visit Yellowstone and experience the world's first national park. Marvel at a volcano's hidden power rising up in colorful hot springs, mudpots, and geysers. Explore mountains, forests, and lakes to watch wildlife and witness the drama of the natural world unfold. Overall, we give this place a 5/5 stars. If you're going to visit a national park, atleast make it the first one."
+
+}
+document.getElementById("YNP").onclick = function(){
+    document.getElementById("prk_header").innerHTML = "Yosemite National Park";
+    document.getElementById("prk_info").innerHTML = "Not just a great valley, but a shrine to human foresight, the strength of granite, the power of glaciers, the persistence of life, and the tranquility of the High Sierra. First protected in 1864, Yosemite National Park is best known for its waterfalls, but within its nearly 1,200 square miles, you can find deep valleys, grand meadows, ancient giant sequoias, a vast wilderness area, and much more. Overall, Yosemite earns a 4.5/5 stars for the sheer beauty of the park."
+}
+document.getElementById("CMTSP").onclick = function(){
+    document.getElementById("gear_head").innerHTML = "Cabela's Mountain Trapper Sleeping Bag";
+    document.getElementById("gear_info").innerHTML = "This sleeping bag provides the perfect mix between weight and comfortability for your average camper. While this may not be the most optimal backpacking bag, the Trapper Sleeping Bag will work great for your average camper. Pick up this bag at your local Cabelas for $129.99."
+}
+document.getElementById("MHT").onclick = function(){
+    document.getElementById("gear_head").innerHTML = "Marmot Halo Tent";
+    document.getElementById("gear_info").innerHTML = "The innovative Halo pole structure provides terrific head room to create a uniquely livable tent for family car camping and outdoor living. Perfect for the casual family outing or the extended trip to the Rocky Mountains of Colorado, this tent truly lives up to the 'there and back again' motto. Pick up this tent for $429.00."
+}
+document.getElementById("BAMBH").onclick = function(){
+    document.getElementById("gear_head").innerHTML = "Bugs Away Mesh Brim Hat";
+    document.getElementById("gear_info").innerHTML = "Keep both the sun and the bugs away with this high tech hat! This hat incorporates a special fabric to keep those bugs away for good! Pick up this fantastic buy for $20.00."
+}
+document.getElementById("RCEPK").onclick = function(){
+    document.getElementById("gear_head").innerHTML = "Red Cross Deluxe Emergency Preparedness Kit";
+    document.getElementById("gear_info").innerHTML = "No matter how good you think your plan is, something is going to go wrong. Always be prepared to offer aid with this emergency preparedness kit by Red Cross. Pick up this full kit for $99.95."
+}
+document.getElementById("NWB").onclick = function(){
+    document.getElementById("gear_head").innerHTML = "Nalgene Water Bottle";
+    document.getElementById("gear_info").innerHTML = "The water bottle that will truly get you there and back again. The generic nalgene is still the king of the kings when it comes to water bottles, providing the ultimate source of beverage for your next adventure. Pick up this bottle for $9.95."
+}
+document.getElementById("CEO").onclick = function(){
+    document.getElementById("job_title").innerHTML = "Chief Executive Officer";
+    document.getElementById("job_desc").innerHTML = "So, you think you've got what it takes to be the CEO of Camptech? Do you enjoy working with other people? Are you willing to work way too hard just for some tiny website that nobody visits? Do you have atleast a PhD in something other than psychology? If so, then you are the right person for the job. The CEO is tasked with running the company and making sure everything goes the way it is supposed to. If interested in applying, send an email to 'thisisnotarealemail@gmail.com'."
+}
+document.getElementById("CFO").onclick = function(){
+    document.getElementById("job_title").innerHTML = "Chief Financial Officer";
+    document.getElementById("job_desc").innerHTML = "Do you like money? Do you like being a chief? Do you like having power in a company that will problably collapse in the next 3-6 months? If you answered 'yes' to any of these questions, you should apply for the Chief Financial Officer position. As the CFO, you will be responsible for managing all money in and out of the company, and working with important dignitaries from across the world on our establishment of an international takeover. Questions? See information above."
+}
+document.getElementById("Acct.").onclick = function(){
+    document.getElementById("job_title").innerHTML = "Accountant";
+    document.getElementById("job_desc").innerHTML = "Let's be honest. If you're just applying for this because you think accouting is easy, back out now. This job will have you cooking the books like we're an international money laundering company. With 90 hour weeks and completely unsafe working conditions, you'll fall in love with this job as fast as you fall asleep at night. Please note, accountants do not recieve benefits and are considered for all legal purposes 'unpaid interns.' Happy applying!"
+}
+document.getElementById("WD").onclick = function(){
+    document.getElementById("job_title").innerHTML = "Web Developer";
+    document.getElementById("job_desc").innerHTML = "As a web developer, you have the privilage of turning our terrible looking, almost nonfunctioning website into something that may or may not work better. Really, this is the only position we are actualy looking to hire someone for, but if you read down this far we know you're truly committed to something greater than yourself. Apply today!"
+}
+
+
